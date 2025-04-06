5c69511019407ea899aa531d993117262158e765
benidama <benidama02@gmail.com>
2025-04-06 14:21:25 +0000

    Langage

---
+++ src/components/Icons.jsx
@@ -0,0 +1,11 @@
+function LanguageSwitcher() {
+  return (
+    <div className="flex gap-4 text-2xl">
+      <button>🇺🇸</button> {/* English */}
+      <button>🇫🇷</button> {/* French */}
+      <button>🇪🇸</button> {/* Spanish */}
+    </div>
+  );
+}
+
+export default LanguageSwitcher;
