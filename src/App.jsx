1c88c90d78e2ea38b4cb73ee1ed313e81b321394
benidama <benidama02@gmail.com>
2025-04-12 20:04:23 +0000

    Adding all changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Icons from "./components/Icons";
 import Challenges from "./components/Challenges";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,8 +15,8 @@ function App() {
 
   return (
     <>
-      <ToggleTheme />
       <BrowserRouter>
+        <ToggleTheme />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
