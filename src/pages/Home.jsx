fcbf63c5672e266bc782ace2f5c15175c82f01b8
benidama <benidama02@gmail.com>
2025-05-04 18:35:41 +0000

    add changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1>Hello guys, are you ok?</h1>
-      <p className="max-w-screen text-amber-300">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
-        dignissimos aspernatur pariatur officia, consequuntur repellat fugiat
-        enim neque mollitia temporibus ratione cum autem tenetur vero! Porro
-        veritatis quidem atque. Assumenda, nostrum?
-      </p>
     </div>
   );
 };
