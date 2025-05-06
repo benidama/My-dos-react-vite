acf1c83140012ce20b5e5c716217b239c86a965e
benidama <benidama02@gmail.com>
2025-05-06 20:29:49 +0000

    added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,10 @@ function About() {
   return (
     <div>
       <h1 className="text-blue-800">Lorem ipsum dolor sit amet.</h1>
+      <p>
+        Here, there is a big match to watch because it is a very big one which
+        is very full many suprises
+      </p>
     </div>
   );
 }
