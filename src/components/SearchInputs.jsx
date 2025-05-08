2ae2bdfdbf8e320ceabfa89ad6dd3ec0bbbddd4a
benidama <benidama02@gmail.com>
2025-05-08 10:49:03 +0000

    resolving some errors

--- src/components/SearchInputs.jsx
+++ src/components/SearchInputs.jsx
@@ -1,7 +1,11 @@
+import PropTypes from "prop-types";
+
 const SearchInputs = ({ placeholder, Icon }) => {
   return (
     <div className="relative flex items-center">
-      <Icon size={20} className="absolute flex left-2 text-gray-400" />
+      {Icon && (
+        <Icon size={20} className="absolute flex left-2 text-gray-400" />
+      )}
       <input
         type="text"
         className="bg-white rounded-md p-2 md:p-4 w-80 md:w-60 outline-none text-sm pl-7 md:pl-8"
@@ -9,6 +13,11 @@ const SearchInputs = ({ placeholder, Icon }) => {
       />
     </div>
   );
+};
+
+SearchInputs.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  Icon: PropTypes.elementType,
 };
 
 export default SearchInputs;
