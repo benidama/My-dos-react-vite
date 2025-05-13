0c55f41599a731ed75fd8a9ec429799b7d4136fb
benidama <benidama02@gmail.com>
2025-05-13 21:05:46 +0000

    We add

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -7,7 +7,8 @@ const Challenges = () => {
       <h1>I can do my JS challenges here.</h1>
       <Flag code="GB" className="w-[2rem]" />
       <p className="bg-white text-black dark:bg-gray-900 dark:text-white p-4">
-        Hello, I'm styled based on theme! Lorem ipsum ad venum aquas
+        Hello, I'm styled based on theme! Lorem ipsum ad venum aquas, here I
+        need to learn much more in order to know everything I need to know.
       </p>
     </div>
   );
